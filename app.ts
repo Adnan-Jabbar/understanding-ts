a335f6d567ea81255550933954bcbad7bac17adb
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-04-25 23:06:29 +0500

    Arrays Types

--- app.ts
+++ app.ts
@@ -2,9 +2,22 @@
 //     name: string;
 //     age: number;
 // } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+} = {
     name: "Adnan Jabbar",
-    age: 27
+    age: 27,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
