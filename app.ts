b420f78c1dd686faab9a1f4054db7fbe4f86f374
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-04-27 01:16:56 +0500

    Function Types & Callbacks

--- app.ts
+++ app.ts
@@ -19,6 +19,12 @@ function printResult(num: number): undefined {
     return;
 }
 
+// Function Types & Callbacks with void
+function addAndHandler(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
 printResult(add(5, 12));
 
 // Function as Types;
@@ -33,3 +39,8 @@ combineValues = add;
 console.log(combineValues(8, 8));
 
 // let someValue = undefined; // its fine
+
+addAndHandler(10, 20, (result) => {
+    console.log(result);
+    // return result; // might be return if remove the return then no issue.
+});
