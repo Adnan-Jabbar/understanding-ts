030d381f5c88436d2e20d150c2f31a9bbcf0ff56
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-04-27 01:43:53 +0500

    The never Type

--- app.ts
+++ app.ts
@@ -10,3 +10,11 @@ if( typeof userInput === "string") {
 }
 
 // userName = userInput; // !!! ERROR !!! we should to use extra code and using typeof.
+
+// never type nothing to return any things.
+function generateError(message: string, code: number): never {
+    throw {errorMessage: message, errorCode: code};
+    // while (true) {}; // never return any thing like never types.
+}
+
+generateError("An error occurred!", 500);
