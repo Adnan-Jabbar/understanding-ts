dd3a8240c52fa0741044980716a4e3c7120deaed
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-04-27 23:39:03 +0500

    Including & Excluding Files

--- app.ts
+++ app.ts
@@ -1,20 +1,3 @@
-// Unknown like the any type but having diffrence
-let userInput: unknown;
-let userName: string;
+const userName = 'AdnanJabbar';
 
-userInput = 5;
-userInput = "Adnan";
-
-if( typeof userInput === "string") {
-    userName = userInput;
-}
-
-// userName = userInput; // !!! ERROR !!! we should to use extra code and using typeof.
-
-// never type nothing to return any things.
-function generateError(message: string, code: number): never {
-    throw {errorMessage: message, errorCode: code};
-    // while (true) {}; // never return any thing like never types.
-}
-
-generateError("An error occurred!", 500);
+console.log(userName);
