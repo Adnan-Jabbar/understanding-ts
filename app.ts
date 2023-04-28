d6f51452351dbf1c8bad7e898935bb59501fa1ed
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-04-28 23:44:36 +0500

    More Configuration & Compilation Options

--- app.ts
+++
@@ -1,3 +0,0 @@
-const userName = 'AdnanJabbar';
-
-console.log(userName);
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+const button = document.querySelector('button')!;
+
+// a Comment
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
+
+// const mapped = new Map();
