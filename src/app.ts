5eb2784f6e8f0ca14f2b14664e5196bcc27369fe
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-04-29 19:02:17 +0500

    Code Quality Options

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,15 @@ const button = document.querySelector('button');
 
 // a Comment
 
+function add(n1: number, n2: number) {
+    if(n1 + n2 > 0) {
+        return n1 + n2;
+    }
+    return;
+}
+
 function clickHandler(message: string) {
+    // let userName = "Adnan";
     console.log('Clicked! ' + message);
 }
 
