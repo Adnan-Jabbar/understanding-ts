05e57d0e658f7cbcd60d0d74adbbcf64ecaed379
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-05-01 01:29:54 +0500

    Creating Class and compling to js also Constructor Functions and The this Keyword

--- src/app.ts
+++ src/app.ts
@@ -1,87 +1,22 @@
-// const userName = 'Max';
-// // userName = 'Maximilian';
+class Department {
+    name: string;
 
-// let age = 30;
+    constructor (n: string) {
+        this.name = n;
+    }
 
-// age = 29;
-
-// function add(a: number, b: number) {
-//     let result;
-//     result = a + b;
-//     return result;
-// }
-
-// if (age > 20) {
-//     let isold = true;
-// }
-
-// console.log(is0ld);
-
-
-// console.log(result);
-
-// Arrow function 
-
-// const add = (a: number, b: number) => {
-//     return a + b;
-// }
-
-// const add = (a: number, b: number) => a + b;
-
-// // Default function set the value in parameters must set in order to last param.
-// const add = (a: number, b: number = 1) => a + b;
-
-// // const printOutput = (output: string | number) => {
-// //     return console.log(output);
-// // }
-
-// // const printOutput = (output: string | number) => console.log(output);
-
-// const printOutput: (a: number | string) => void = output => console.log(output);
-
-// const button = document.querySelector('button');
-
-// if(button) {
-//     button.addEventListener('click', event => console.log(event));
-// }
-
-// // console.log(add(5, 2));
-// console.log(add(5));
-
-// Spread Operator
-const hobbies = ['Sports','Cooking'];
-const activeHobbies = ['Hiking'];
-
-activeHobbies.push(...hobbies);
-
-const person = {
-    firstName: 'Adnan',
-    age: 27
+    describe(this: Department) {
+        console.log('Department :' + this.name);
+    }
 }
-const coppiedPerson = { ...person };
 
-console.log(coppiedPerson);
+const accounting = new Department('Accounting');
 
-// Rest Parameters
-const add = (...numbers: number[]) => {
-    return numbers.reduce((curResult, curValue) => {
-        return curResult + curValue;
-    }, 0);
-};
+accounting.describe();
 
-const addNumbers = add(5, 10, 2, 3.7);
-console.log(addNumbers);
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-
-console.log(hobbies, hobby1, hobby2);
-
-const { firstName: userName, age } = person;
-
-console.log(userName, age, person);
-
-
-
+accountingCopy.describe();
 
 
 
