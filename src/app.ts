58c442987650c584438adf7e6a57c9d23ff4e4bf
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-04-30 14:27:45 +0500

    Arrow Function and Default Function Parameters

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,51 @@
+const userName = 'Max';
+// userName = 'Maximilian';
+
+let age = 30;
+
+age = 29;
+
+// function add(a: number, b: number) {
+//     let result;
+//     result = a + b;
+//     return result;
+// }
+
+// if (age > 20) {
+//     let isold = true;
+// }
+
+// console.log(is0ld);
+
+
+// console.log(result);
+
+// Arrow function 
+
+// const add = (a: number, b: number) => {
+//     return a + b;
+// }
+
+// const add = (a: number, b: number) => a + b;
+
+// Default function set the value in parameters must set in order to last param.
+const add = (a: number, b: number = 1) => a + b;
+
+// const printOutput = (output: string | number) => {
+//     return console.log(output);
+// }
+
+// const printOutput = (output: string | number) => console.log(output);
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
 const button = document.querySelector('button');
 
-// a Comment
-
-function add(n1: number, n2: number) {
-    if(n1 + n2 > 0) {
-        return n1 + n2;
-    }
-    return;
-}
-
-function clickHandler(message: string) {
-    // let userName = "Adnan";
-    console.log('Clicked! ' + message);
-}
-
 if(button) {
-    button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+    button.addEventListener('click', event => console.log(event));
 }
 
-// const mapped = new Map();
-
+// console.log(add(5, 2
+console.log(add(5));
 
 
