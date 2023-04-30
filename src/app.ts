4c578da483bd5dcabc694e78261a2e1e54b309c1
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-05-01 01:55:46 +0500

    Private and Public Access Modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-    name: string;
+    public name: string;
+    private employees: string[] = [];
 
     constructor (n: string) {
         this.name = n;
@@ -8,15 +9,31 @@ class Department {
     describe(this: Department) {
         console.log('Department :' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printExmployeeInfromation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('Adnan');
+accounting.addEmployee('Imbisat');
+
+// accounting.employees[2] = 'Kamran';
+
 accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printExmployeeInfromation();
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
 
 
 
