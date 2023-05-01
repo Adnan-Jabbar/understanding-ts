1e35052e9463c18e53d225ad816d1867cce07e8d
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-05-01 16:28:30 +0500

    Getters & Setters

--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found.')
+    }
+
+    set mostRecentReport(value: string) {
+        if(!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
+    }
+
     constructor(id: string, private reports: string[]) {
         super(id, 'Accounting');
+        this.lastReport = reports[0];
     }
 
     addEmployee(name: string) {
@@ -46,6 +63,7 @@ class AccountingDepartment extends Department {
     
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
 
     printReport() {
@@ -70,7 +88,10 @@ console.log(it);
 
 const accounting = new AccountingDepartment('D2', []);
 
+accounting.mostRecentReport = 'Year End Report';
 accounting.addReport('Something went wrong...');
+// Getter method will always call like as property not like function.
+console.log(accounting.mostRecentReport);
 
 accounting.addEmployee('Adnan');
 accounting.addEmployee('Zeeshan');
