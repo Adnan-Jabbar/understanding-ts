74893362e548d93e0aad0fa8cd10b4c7e613f1bd
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-05-01 18:42:51 +0500

    Singletons & Private Constructors

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,7 @@ class ITDepartment extends Department {
 
 class AccountingDepartment extends Department {
     private lastReport: string;
+    private static instance: AccountingDepartment;
 
     get mostRecentReport() {
         if (this.lastReport) {
@@ -60,9 +61,19 @@ class AccountingDepartment extends Department {
         this.addReport(value);
     }
 
-    constructor(id: string, private reports: string[]) {
+    // Singletones & Private Constructor
+    private constructor(id: string, private reports: string[]) {
         super(id, 'Accounting');
         this.lastReport = reports[0];
+    }
+
+    // singletones instance.
+    static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('D2', []);
+        return this.instance;
     }
 
     describe() {
@@ -104,7 +115,11 @@ it.printExmployeeInfromation();
 
 console.log(it);
 
-const accounting = new AccountingDepartment('D2', []);
+// const accounting = new AccountingDepartment('D2', []);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+
+console.log(accounting, accounting2);
 
 accounting.mostRecentReport = 'Year End Report';
 accounting.addReport('Something went wrong...');
