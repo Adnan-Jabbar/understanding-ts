ec7045da2a97d5154362d00f4fd47196fe6dbbd0
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-05-01 15:33:31 +0500

    Overriding Properties And The protected Modifier

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class Department {
     // private readonly id: string;
     // public name: string;
-    private employees: string[] = [];
+    protected employees: string[] = [];
 
     constructor (private readonly id: string, public name: string) {
         // this.id = id;
@@ -37,6 +37,13 @@ class AccountingDepartment extends Department {
         super(id, 'Accounting');
     }
 
+    addEmployee(name: string) {
+        if(name === "Adnan") {
+            return;
+        }
+        this.employees.push(name);
+    }
+    
     addReport(text: string) {
         this.reports.push(text);
     }
@@ -65,7 +72,11 @@ const accounting = new AccountingDepartment('D2', []);
 
 accounting.addReport('Something went wrong...');
 
+accounting.addEmployee('Adnan');
+accounting.addEmployee('Zeeshan');
+
 accounting.printReport();
+accounting.printExmployeeInfromation();
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
