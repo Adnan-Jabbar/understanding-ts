ce2c5f9ef2e9a5a3dfe541d08016ce3abf496be4
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-05-07 22:49:51 +0500

    Working with Decorator Factories

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
+// Working with Decorator Factories
 // function Logger(target: Function as Argument) {
-function Logger(constructor: Function) {
-    console.log('Logging...');
-    console.log(constructor);
-}
+function Logger(logString: string) {
+    return function (constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    };
+};
 
-@Logger
+@Logger('LOGGING - PERSON')
 class Person {
     name = 'Adnan';
 
