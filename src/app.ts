5a30a7f9c4a3602d192c555a5e0c57ac0d99e9f2
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-05-07 13:54:32 +0500

    Working with Constraints

--- src/app.ts
+++ src/app.ts
@@ -14,21 +14,29 @@
 //     // data.split(' ');
 // });
 
-// function merge(objA: object, objB: object) {
-// function merge(objA: {name: string, hobbies: string[]}, objB: { age: number}) {
-// function merge<T extends { name: string, hobbies: string[]}, U>(objA: T, objB: U) {
-function merge<T extends {}, U>(objA: T, objB: U) {
+/**** Creating a Generic Function ****/
+// // function merge(objA: object, objB: object) {
+// // function merge(objA: {name: string, hobbies: string[]}, objB: { age: number}) {
+// // function merge<T extends { name: string, hobbies: string[]}, U>(objA: T, objB: U) {
+// function merge<T, U>(objA: T, objB: U) {
+//     return Object.assign(objA, objB);
+// }
+
+// // const mergedObj = merge<T, U>({ name: 'Adnan', hobbies: ['Sports'] }, { age: 30 });
+// // const mergedObj = merge<string, number>({ name: 'Adnan' }, { age: 30 });
+// // const mergedObj = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Adnan', hobbies: ['Sports']}, {age: 30});
+// const mergedObj = merge({name: 'Adnan', hobbies: ['Sports']}, 30);
+
+// console.log(mergedObj);
+
+/**** Working with Constraints ****/
+function merge<T extends Object, U extends Object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
-// const mergedObj = merge<T, U>({ name: 'Adnan', hobbies: ['Sports'] }, { age: 30 });
-// const mergedObj = merge<string, number>({ name: 'Adnan' }, { age: 30 });
-// const mergedObj = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Adnan', hobbies: ['Sports']}, {age: 30});
 const mergedObj = merge({name: 'Adnan', hobbies: ['Sports']}, { age: 30 });
 
 console.log(mergedObj);
-
-
 
 
 
