c840f2f01236e95a57c59a32e5ef53a76894db16
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-05-13 18:40:27 +0500

    Example Creating an Autobind Decorator

--- src/app.ts
+++ src/app.ts
@@ -94,5 +94,37 @@ class Product {
     }
 }
 
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This work!';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+// p.showMessage();
+
+const button = document.querySelector('button')!;
+// button.addEventListener('click', p.showMessage.bind(p));
+button.addEventListener('click', p.showMessage);
+
+
+
+
 
 
